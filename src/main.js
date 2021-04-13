dd63b85119a39e5663903f38c9a3e08f831bf765
Jesse White <jesse.white6@gmail.com>
2021-04-13 16:06:28 -0700

    add user logic to populate on page.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Triangle from './triangle.js';
 
 $(document).ready(function () {
+    ("form").submit(function (event) {
+        event.preventDefault();
 
+        const lengthOne = $("#side1").val();
+        const lengthTwo = $("#side2").val();
+        const lengthThree = $("#side3").val();
+
+        const triangle = new Triangle(lengthOne, lengthTwo, lengthThree);
+
+        const response = triangle.checkType();
+        $("#response").append(`<p>${response}</p>`);
+
+    });
 });
